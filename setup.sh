dd41dd41dc31e19a34c4ae5294cb21bc90ae0aa1
tseimandi <tom.seimandi@insee.fr>
2022-04-13 15:17:53 +0000

    Add root to PYTHONPATH

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 pip install -r requirements.txt
 pre-commit install
 
+export PYTHONPATH="$PWD:$PYTHONPATH"
 export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY:$AWS_SESSION_TOKEN@$AWS_S3_ENDPOINT
 mc cp -r minio/projet-ape/data/ data/
--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+"""
+Main script.
+"""
+
+
 def main():
     pass
 
