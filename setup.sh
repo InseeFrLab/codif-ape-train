a8c36379da84d0e06627c1b4c309ef85fe40c06a
tseimandi <tom.seimandi@insee.fr>
2022-04-13 14:32:58 +0000

    Add pre-commit install instruction to set up script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
+pre-commit install
 
 export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY:$AWS_SESSION_TOKEN@$AWS_S3_ENDPOINT
 mc cp -r minio/projet-ape/data/ data/
