f8e0040b396700ec0dc998c0d08fe2c00c3ef3c0
tfaria <thomas.faria@ensae.fr>
2022-07-13 07:58:33 +0000

    improve prepro [NO CHECKED YET]

--- src/fasttext_classifier/fasttext_preprocessor.py
+++ src/fasttext_classifier/fasttext_preprocessor.py
@@ -4,6 +4,7 @@ FastTextPreprocessor class.
 import string
 from typing import List, Optional, Tuple
 
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -57,34 +58,83 @@ class FastTextPreprocessor(Preprocessor):
 
         return df_train, df_test, df_gu
 
-    def clean_lib(self, df: pd.DataFrame, idx: int, text_feature: str) -> str:
+    def clean_lib(self, df: pd.DataFrame, text_feature: str) -> pd.DataFrame:
         """
         Cleans a text feature for pd.DataFrame `df` at index idx.
 
         Args:
             df (pd.DataFrame): DataFrame.
-            idx (int): Index.
             text_feature (str): Name of the text feature.
 
         Returns:
-            str: Cleaned text.
+            df (pd.DataFrame): DataFrame.
         """
+        # On définit 2 Regex de mots à supprimer du jeu de données
+        LongWord2remove = r"\bconforme au kbis\b|\bsans changement\b|\bsans acitivite\b|\bactivite inchangee\b|\bactivites inchangees\b|\bsiege social\b|\ba definir\b|\ba preciser\b|\bci dessus\b|\bci desus\b|\bvoir activit principale\b|\bvoir activite principale\b|\bvoir objet social\b|\bidem extrait kbis\b|\bidem cadre precedent\b|\bn a plus a etre mentionne sur l extrait decret\b|\bcf statuts\b|\bactivite principale case\b|\bactivites principales case\b|\bactivite principale\b|\bactivites principales\b|\bidem case\b|\bvoir case\b|\baucun changement\b|\bsans modification\b|\bactivite non modifiee\b"
+        Word2remove = r"\bcode\b|\bcadre\b|\bape\b|\bape[a-z]{1}\b|\bnaf\b|\binchangee\b|\binchnagee\b|\bkbis\b|\bk bis\b|\binchangees\b|\bnp\b|\binchange\b|\bnc\b|\bidem\b|\bxx\b|\bxxx\b"
+
+        # On passe tout en minuscule
+        df[text_feature] = df[text_feature].map(str.lower)
+
         # On supprime toutes les ponctuations
-        lib = df.at[idx, text_feature].translate(
-            str.maketrans(string.punctuation, " " * len(string.punctuation))
+        df[text_feature] = df[text_feature].replace(
+            to_replace=r"[^\w\s]", value=" ", regex=True
         )
+
         # On supprime tous les chiffres
-        lib = lib.translate(str.maketrans(string.digits, " " * len(string.digits)))
+        df[text_feature] = df[text_feature].replace(
+            to_replace=r"[\d+]", value=" ", regex=True
+        )
 
-        # On supprime les stopwords, on renvoie les mots en minuscule
-        lib = [x.lower() for x in lib.split() if x.lower() not in self.stopwords]
+        # On supprime les longs mots sans sens
+        df[text_feature] = df[text_feature].replace(
+            to_replace=LongWord2remove, value="", regex=True
+        )
 
-        # On récupère la racine de chaque mot
-        lib = [self.stemmer.stem(word) for word in lib]
+        # On supprime les mots courts sans sens
+        df[text_feature] = df[text_feature].replace(
+            to_replace=Word2remove, value="", regex=True
+        )
 
-        # On supprime les mots dupliqués dans un même libellé
-        lib_clean = " ".join(sorted(set(lib), key=lib.index))
-        return lib_clean
+        # On supprime les mots d'une seule lettre
+        df[text_feature] = df[text_feature].replace(
+            to_replace=r"\b[a-z]{1}\b", value="", regex=True
+        )
+
+        # On supprime les multiple space
+        df[text_feature] = df[text_feature].replace(r"\s\s+", " ", regex=True)
+
+        # On strip les libellés
+        df[text_feature] = df[text_feature].str.strip()
+
+        # On remplace les empty string par des NaN
+        df[text_feature] = df[text_feature].replace(r"^\s*$", np.nan, regex=True)
+
+        # On supprime les NaN
+        df = df.dropna(subset=[text_feature])
+
+        # On tokenize tous les libellés
+        libs_token = [lib.split() for lib in df[text_feature].to_list()]
+
+        # On supprime les mots duppliqué dans un même libellé
+        libs_token = [
+            sorted(set(libs_token[i]), key=libs_token[i].index)
+            for i in range(len(libs_token))
+        ]
+
+        # Pour chaque libellé on supprime les stopword et on racinise les mots
+        df[text_feature] = [
+            " ".join(
+                [
+                    self.stemmer.stem(word)
+                    for word in libs_token[i]
+                    if word not in self.stopwords
+                ]
+            )
+            for i in range(len(libs_token))
+        ]
+
+        return df
 
     def get_aggregated_APE(
         self,
