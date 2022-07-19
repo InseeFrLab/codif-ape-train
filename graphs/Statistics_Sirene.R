61e840d6815b70a046e29fa94a9cf3849c569a2a
tfaria <thomas.faria@ensae.fr>
2022-07-19 16:18:06 +0200

    ajusting the harmonisation of codes

--- graphs/Statistics_Sirene.R
+++ graphs/Statistics_Sirene.R
@@ -14,6 +14,7 @@ aws.s3::get_bucket("projet-ape", region = "", prefix = "data/extraction_sirene_2
 
 
 ##### Data importation #####
+# Import the main database
 df <- 
   aws.s3::s3read_using(
     FUN = arrow::read_parquet,
@@ -23,17 +24,16 @@ df <-
     opts = list("region" = "")
   )
 
+# Import the table with all APE codes
 naf <- read_csv("~/codification-ape/data/naf_extended.csv")
-
 naf <- naf  %>% 
   mutate_all(~sub("\\.", "", .))%>%
   rename(APE_SICORE_NEW = NIV5)
 
-# e ####
+##### Preprocessing #####
 LongWord2remove = "\\bconforme au kbis\\b|\\bsans changement\\b|\\bsans activite\\b|\\bsans acitivite\\b|\\bactivite inchangee\\b|\\bactivites inchangees\\b|\\bsiege social\\b|\\ba definir\\b|\\ba preciser\\b|\\bci dessus\\b|\\bci desus\\b|\\bci desssus\\b|\\bvoir activit principale\\b|\\bvoir activite principale\\b|\\bvoir objet social\\b|\\bidem extrait kbis\\b|\\bn a plus a etre mentionne sur l extrait decret\\b|\\bcf statuts\\b|\\bactivite principale case\\b|\\bactivites principales case\\b|\\bactivite principale\\b|\\bactivites principales\\b|\\bidem case\\b|\\bvoir case\\b|\\baucun changement\\b|\\bsans modification\\b|\\bactivite non modifiee\\b|\\bactivite identique\\b|\\bpas de changement\\b|\\bpas de changement.{0,6}activite\\b"
 Word2remove = "|\\bcode\\b|\\bape\\b|\\bape[a-z]{1}\\b|\\bnaf\\b|\\binchangee\\b|\\binchanges\\b|\\binchnagee\\b|\\bkbis\\b|\\bk bis\\b|\\binchangees\\b|\\bnp\\b|\\binchange\\b|\\bnc\\b|\\bidem\\b|\\bxx\\b|\\bxxx\\b|\\binconnue\\b|\\binconnu\\b|\\bvoir\\b|\\bannexe\\b"
 
-#voir activite principale
 df_ <-df%>%
   mutate(LIB_SICORE_NEW = tolower(LIB_SICORE),
          LIB_SICORE_NEW = str_replace_all(LIB_SICORE_NEW, "[:punct:]", " "), #Enleve ponctuation, strip le text, remplace "" par NAN
@@ -43,14 +43,14 @@ df_ <-df%>%
          LIB_SICORE_NEW = na_if(str_squish(LIB_SICORE_NEW), "")
          )%>%
   drop_na(APE_SICORE, LIB_SICORE_NEW)%>% #suppr les NAN
-  select(LIA_NUM,DATE, APE_SICORE, LIB_SICORE, LIB_SICORE_NEW, LIB_LIASSE_ETAB_E71, EVT_SICORE, AUTO, NAT_SICORE, SURF)
+  select(LIA_NUM,DATE, APE_SICORE, LIB_SICORE, LIB_SICORE_NEW, EVT_SICORE, AUTO, NAT_SICORE, SURF)
 
-
+##### Enhancing the libs #####
+# Get all duplicated libs
 LibDuplicated <- df_%>%subset(duplicated(LIB_SICORE_NEW))%>%pull(LIB_SICORE_NEW)%>%unique()
-
 df_LibDuplicated <- df_%>%subset(LIB_SICORE_NEW %in% LibDuplicated)
 
-# Pour chaque libellé on regarde le nombre de code APE différent
+# We create a new APE code when there is a different code for the same lib
 HarmonizedAPE <- df_LibDuplicated%>%
   group_by(LIB_SICORE_NEW)%>%
   summarize(
@@ -59,27 +59,42 @@ HarmonizedAPE <- df_LibDuplicated%>%
          N = n(),
          Prop = NbDiff/N,
   )%>%
-  filter((NbDiff>1) & (Prop<0.8))
+  filter((NbDiff>1) & (Prop<0.8))%>%
+  select(LIB_SICORE_NEW, APE_SICORE_NEW)
 
 
-www <- df_%>%left_join(HarmonizedAPE)%>%
+#df_new <- df_ %>%left_join(HarmonizedAPE)%>%
+#  mutate(APE_SICORE_NEW = case_when(is.na(APE_SICORE_NEW) ~ APE_SICORE,
+#                                    T ~ APE_SICORE_NEW)
+#  )%>%
+#  left_join(naf, by = "APE_SICORE_NEW")%>%
+#  select(LIA_NUM,DATE, APE_SICORE, LIB_SICORE, LIB_SICORE_NEW, EVT_SICORE, AUTO, NAT_SICORE, SURF, NIV1, NIV2, NIV3, NIV4, LIB_NIV5)
+
+##### Saving the new database with harmonized codes #####
+
+df_new <- df_ %>%left_join(HarmonizedAPE)%>%
   mutate(APE_SICORE_NEW = case_when(is.na(APE_SICORE_NEW) ~ APE_SICORE,
                                     T ~ APE_SICORE_NEW)
-  )
-  
-ss<-www%>%filter(APE_SICORE_NEW != APE_SICORE)
+  )%>%
+  select(LIA_NUM,DATE, APE_SICORE_NEW, LIB_SICORE, EVT_SICORE, AUTO, NAT_SICORE, SURF)%>%
+  rename(APE_SICORE = APE_SICORE_NEW)
+
+aws.s3::s3write_using(
+  df_new,
+  FUN = arrow::write_parquet,
+  # Mettre les options de FUN ici
+  multipart = TRUE,
+  object = "/data/extraction_sirene_20220712_harmonised.parquet",
+  bucket = "projet-ape",
+  opts = list("region" = "")
+)
 
 
-# Cleaning the dataset for the preprocessing
-# run model with 1 digit and 3 digit
-# rewrite APE code for same libs
+##### Creating an oversampled database #####
+
 # faire des stats sur les classes
 # faire oversampling seuil à 5000 ou 2500
-
-
-www <- www%>%
-  left_join(naf, by = "APE_SICORE_NEW")%>%
-  select(LIA_NUM,DATE, APE_SICORE, LIB_SICORE, LIB_SICORE_NEW, LIB_LIASSE_ETAB_E71, EVT_SICORE, AUTO, NAT_SICORE, SURF)
+# s'assurer que le split est bien fait
 
 qq <- www%>%count(APE_SICORE_NEW)
 qq_NEW <- www%>%count(APE_SICORE)
