e6b5c51b977fb7dc1a48bd876a5e9b9143a18821
tfaria <thomas.faria@ensae.fr>
2022-07-20 12:09:18 +0000

    adding new config files

--- bash/mlflow-run.sh
+++ bash/mlflow-run.sh
@@ -16,5 +16,5 @@ done <<< "$GET_PODS"
 export MLFLOW_TRACKING_URI="https://projet-ape-$POD_ID.kub.sspcloud.fr"
 export MLFLOW_EXPERIMENT_NAME="test"
 
-mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P data_path=data/extraction_sirene_20220712.parquet -P config_path=config/config_fasttext27.yaml
-mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P data_path=data/extraction_sirene_20220712.parquet -P config_path=config/config_fasttext33.yaml
+mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P data_path=data/extraction_sirene_20220712_harmonised.parquet -P config_path=config/config_fasttext27.yaml
+mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P data_path=data/extraction_sirene_20220712_harmonised.parquet -P config_path=config/config_fasttext33.yaml
