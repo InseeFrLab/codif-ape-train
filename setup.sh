a30659b5eba2541c704be44adb201c9dda3db061
tfaria <thomas.faria@ensae.fr>
2022-07-28 11:59:05 +0000

    removing s3 token, we dont need it anymore

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 pre-commit install
 
 export PYTHONPATH="$PWD:$PYTHONPATH"
-export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY:$AWS_SESSION_TOKEN@$AWS_S3_ENDPOINT
+export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
 mc cp -r minio/projet-ape/data/extraction_sirene_20220712_harmonised.parquet data/
 mc cp -r minio/projet-ape/data/extraction_sirene_20220712.parquet data/
 mc cp -r minio/projet-ape/data/naf_extended.csv data/
