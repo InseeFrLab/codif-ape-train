48eece6d35cc72de61111d4fdc8910cb37bd47f9
tfaria <thomas.faria@insee.fr>
2022-10-10 14:30:10 +0000

    add comparison with other df

--- notebooks/parse_log.py
+++ notebooks/parse_log.py
@@ -1,7 +1,13 @@
 import sys
 import pandas as pd
-from enum import Enum
+import unidecode
 import re
+import fasttext
+import numpy as np
+from nltk.corpus import stopwords as ntlk_stopwords
+from nltk.stem.snowball import SnowballStemmer
+from enum import Enum
+
 sys.path.append("../")
 
 HEADER_LEN = 57
@@ -79,6 +85,86 @@ def extract_first_pred(predictions):
     return (first_code, second_code, float(first_proba), float(second_proba))
 
 
+def clean_lib(df, text_feature):
+        """
+        Cleans a text feature for pd.DataFrame `df` at index idx.
+
+        Args:
+            df (pd.DataFrame): DataFrame.
+            text_feature (str): Name of the text feature.
+
+        Returns:
+            df (pd.DataFrame): DataFrame.
+        """
+        # On définit 2 Regex de mots à supprimer du jeu de données
+        LongWord2remove = r"\bconforme au kbis\b|\bsans changement\b|\bsans acitivite\b|\bactivite inchangee\b|\bactivites inchangees\b|\bsiege social\b|\ba definir\b|\ba preciser\b|\bci dessus\b|\bci desus\b|\bvoir activit principale\b|\bvoir activite principale\b|\bvoir objet social\b|\bidem extrait kbis\b|\bidem cadre precedent\b|\bn a plus a etre mentionne sur l extrait decret\b|\bcf statuts\b|\bactivite principale case\b|\bactivites principales case\b|\bactivite principale\b|\bactivites principales\b|\bidem case\b|\bvoir case\b|\baucun changement\b|\bsans modification\b|\bactivite non modifiee\b"
+        Word2remove = r"\bcode\b|\bcadre\b|\bape\b|\bape[a-z]{1}\b|\bnaf\b|\binchangee\b|\binchnagee\b|\bkbis\b|\bk bis\b|\binchangees\b|\bnp\b|\binchange\b|\bnc\b|\bidem\b|\bxx\b|\bxxx\b"
+
+        # On passe tout en minuscule
+        df[text_feature] = df[text_feature].map(str.lower)
+
+        # On supprime toutes les ponctuations
+        df[text_feature] = df[text_feature].replace(
+            to_replace=r"[^\w\s]", value=" ", regex=True
+        )
+
+        # On supprime tous les chiffres
+        df[text_feature] = df[text_feature].replace(
+            to_replace=r"[\d+]", value=" ", regex=True
+        )
+
+        # On supprime les longs mots sans sens
+        #df[text_feature] = df[text_feature].replace(
+        #    to_replace=LongWord2remove, value="", regex=True
+        #)
+
+        # On supprime les mots courts sans sens
+        #df[text_feature] = df[text_feature].replace(
+        #    to_replace=Word2remove, value="", regex=True
+        #)
+
+        # On supprime les mots d'une seule lettre
+        df[text_feature] = df[text_feature].replace(
+            to_replace=r"\b[a-z]{1}\b", value="", regex=True
+        )
+
+        # On supprime les multiple space
+        df[text_feature] = df[text_feature].replace(r"\s\s+", " ", regex=True)
+
+        # On strip les libellés
+        df[text_feature] = df[text_feature].str.strip()
+
+        # On remplace les empty string par des NaN
+        df[text_feature] = df[text_feature].replace(r"^\s*$", np.nan, regex=True)
+
+        # On supprime les NaN
+        # df = df.dropna(subset=[text_feature])
+        df[text_feature] =df[text_feature].fillna(value="NaN")
+
+        # On tokenize tous les libellés
+        libs_token = [lib.split() for lib in df[text_feature].to_list()]
+
+        # On supprime les mots duppliqué dans un même libellé
+        libs_token = [
+            sorted(set(libs_token[i]), key=libs_token[i].index)
+            for i in range(len(libs_token))
+        ]
+
+        # Pour chaque libellé on supprime les stopword et on racinise les mots
+        df[text_feature] = [
+            " ".join(
+                [
+                    stemmer.stem(word)
+                    for word in libs_token[i]
+                    if word not in stopwords
+                ]
+            )
+            for i in range(len(libs_token))
+        ]
+
+        return df
+
+
 RAW_INPUT_FIELDS = [
     "norme",
     "siren",
@@ -134,6 +220,7 @@ if __name__ == "__main__":
         [
             parse_raw_input(info_input, INFO_FIELDS, INFO_REGEXES)
             for info_input in df_info.description
+            if not info_input.__contains__('""')
         ]
     )
 
@@ -145,3 +232,36 @@ if __name__ == "__main__":
     df["second_pred"] = [prediction[1] for prediction in predictions]
     df["first_proba"] = [prediction[2] for prediction in predictions]
     df["second_proba"] = [prediction[3] for prediction in predictions]
+
+
+    model = fasttext.load_model("../models/model.bin")
+
+    tmp = model.predict(df.iloc[:]["libelleNettoye"].to_list(), k=2)
+
+    dict_results = {f"pred_{pred+1}" : [tmp[0][lib][pred][-5:] for lib in range(len(tmp[0]))]
+    for pred in range(2)
+    } | {
+    f"prob_{pred+1}" : [tmp[1][lib][pred] for lib in range(len(tmp[0]))]
+    for pred in range(2)
+    }
+
+    df_results = pd.DataFrame(dict_results)
+    df_results
+
+    sum(df.first_pred == df_results.pred_1)/df.shape[0]
+
+
+    stemmer = SnowballStemmer(language="french")
+    stopwords = tuple(ntlk_stopwords.words("french"))
+
+    df["libelleActivite"] = [unidecode.unidecode(lib) for lib in df["libelleActivite"]]
+
+    dff = df.copy()
+    dff["libelleActivite"] = df["libelleActivite"].fillna(value="NaN")
+    df_prepro = clean_lib(dff, "libelleActivite")
+    lib_raw = df.libelleActivite.to_list()
+    lib_clean_PY = df_prepro.libelleActivite.to_list()
+    lib_clean_JAVA = df.libelleNettoye.apply(lambda x : x.split(" AUTO")[0]).to_list()
+    compare_libs = pd.DataFrame({"RAW" : lib_raw, "PYTHON" : lib_clean_PY, "JAVA" : lib_clean_JAVA})
+    compare_libs["CHECK"] = compare_libs.PYTHON ==  compare_libs.JAVA
+    compare_libs
