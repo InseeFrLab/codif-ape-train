a3d6f288628e8e713e833b0d0f70499100efa344
tfaria <thomas.faria@insee.fr>
2022-10-14 16:50:08 +0000

    using the new database

--- setup.sh
+++ setup.sh
@@ -2,15 +2,12 @@
 sudo apt-get update
 sudo apt-get install p7zip*
 
-git clone https://oauth2:$GITLAB_TOKEN@git.lab.sspcloud.fr/ssplab/codification-ape.git
-cd codification-ape
-
 pip install -r requirements.txt
 pre-commit install
 
 export PYTHONPATH="$PWD:$PYTHONPATH"
 export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-mc cp -r minio/projet-ape/data/extraction_sirene_20220712_harmonised.parquet data/
+mc cp -r minio/projet-ape/data/extraction_sirene_20220712_harmonised_20221014.parquet data/
 mc cp -r minio/projet-ape/data/extraction_sirene_20220712.parquet data/
 mc cp -r minio/projet-ape/data/naf_extended.csv data/
 mc cp minio/projet-ape/api_log.7z data/
