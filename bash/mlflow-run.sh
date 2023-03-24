eeac600b68d609ef3ca8140baf31805ed9769218
tfaria <thomas.faria@insee.fr>
2023-03-24 17:31:45 +0000

    hard coded mlflow URI => conseil d'Olivier

--- bash/mlflow-run.sh
+++ bash/mlflow-run.sh
@@ -1,19 +1,11 @@
 #! /bin/bash
+# Set MLFLOW_EXPERIMENT_NAME environment variable
 export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
 
 # Set MLFLOW_TRACKING_URI environment variable
-GET_PODS=`kubectl get pods`
+export MLFLOW_TRACKING_URI="https://projet-ape-537858.user.lab.sspcloud.fr"
 
-while IFS= read -r line; do
-    VAR=`echo "${line}" | sed -n "s/.*mlflow-\([0-9]\+\)-.*/\1/p"`
-    if [ -z "$VAR" ]; then
-        :
-    else
-        POD_ID=$VAR
-    fi
-done <<< "$GET_PODS"
-
-export MLFLOW_TRACKING_URI="https://projet-ape-$POD_ID.kub.sspcloud.fr"
-export MLFLOW_EXPERIMENT_NAME="Production"
+# Set MLFLOW_EXPERIMENT_NAME environment variable
+export MLFLOW_EXPERIMENT_NAME="test"
 
 mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P data_path=data/extraction_sirene_20220712_harmonized_20221014.parquet -P config_path=config/config_fasttext27.yaml
