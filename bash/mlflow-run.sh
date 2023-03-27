a6a794b2215004e1e4ccd3cae788f142c22dc359
tfaria <thomas.faria@insee.fr>
2023-03-27 12:45:07 +0000

    add experiment name in mlflow run

--- bash/mlflow-run.sh
+++ bash/mlflow-run.sh
@@ -6,6 +6,6 @@ export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
 export MLFLOW_TRACKING_URI="https://projet-ape-537858.user.lab.sspcloud.fr"
 
 # Set MLFLOW_EXPERIMENT_NAME environment variable
-export MLFLOW_EXPERIMENT_NAME="test"
+export MLFLOW_EXPERIMENT_NAME="Production"
 
-mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P data_path=data/extraction_sirene_20220712_harmonized_20221014.parquet -P config_path=config/config_fasttext27.yaml
+mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME -P data_path=data/extraction_sirene_20220712_harmonized_20221014.parquet -P config_path=config/config_fasttext27.yaml
