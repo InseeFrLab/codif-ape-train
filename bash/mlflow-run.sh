38d92ab6ad792032b1b31b238393f189ad60cb5d
tfaria <thomas.faria@insee.fr>
2023-03-29 08:42:55 +0000

    add env var

--- bash/mlflow-run.sh
+++ bash/mlflow-run.sh
@@ -8,4 +8,11 @@ export MLFLOW_TRACKING_URI="https://projet-ape-537858.user.lab.sspcloud.fr"
 # Set MLFLOW_EXPERIMENT_NAME environment variable
 export MLFLOW_EXPERIMENT_NAME="Production"
 
-mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME -P data_path=data/extraction_sirene_20220712_harmonized_20221014.parquet -P config_path=config/config_fasttext27.yaml
+# Set DATA_PATH environment variable
+export DATA_PATH="data/data_sirene3.parquet"
+
+# Set CONFIG_PATH environment variable
+export CONFIG_PATH="config/config_fasttext27.yaml"
+
+
+mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME -P data_path=$DATA_PATH -P config_path=$CONFIG_PATH
