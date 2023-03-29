07300c98c345743d73111415656d1fc93f2b2835
tfaria <thomas.faria@insee.fr>
2023-03-29 13:22:17 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -9,9 +9,7 @@ pre-commit install
 
 export PYTHONPATH="$PWD:$PYTHONPATH"
 #export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-mc cp -r s3/projet-ape/data/extraction_sirene_20220712_harmonized_20221014.parquet data/
-mc cp -r s3/projet-ape/data/extraction_sirene_20220712.parquet data/
-mc cp -r s3/projet-ape/data/naf_extended.csv data/
+mc cp s3/projet-ape/data/data/data_sirene3.parquet data/data_sirene3.parquet
 mc cp s3/projet-ape/20230105_logs.zip data/
 
 7z x data/20230105_logs.zip -odata/
