bac638177145942c81245a6b4e7c3ee9a84ba988
tfaria <thomas.faria@insee.fr>
2023-03-30 09:13:14 +0000

    correct typo

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ pre-commit install
 
 export PYTHONPATH="$PWD:$PYTHONPATH"
 #export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-mc cp s3/projet-ape/data/data/data_sirene3.parquet data/data_sirene3.parquet
+mc cp s3/projet-ape/data/data_sirene3.parquet data/data_sirene3.parquet
 mc cp s3/projet-ape/20230105_logs.zip data/
 
 7z x data/20230105_logs.zip -odata/
