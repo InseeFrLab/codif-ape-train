c33988ab2f996728349cd9d768fd2c56616d5e2d
tfaria <thomas.faria@insee.fr>
2023-04-03 15:31:43 +0000

    add script that fecth and test a specific model

--- bash/mlflow-run.sh
+++ bash/mlflow-run.sh
@@ -3,7 +3,7 @@
 export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
 
 # Set MLFLOW_TRACKING_URI environment variable
-export MLFLOW_TRACKING_URI="https://projet-ape-496146.user.lab.sspcloud.fr"
+export MLFLOW_TRACKING_URI="https://projet-ape-4549.user.lab.sspcloud.fr"
 
 # Set MLFLOW_EXPERIMENT_NAME environment variable
 export MLFLOW_EXPERIMENT_NAME="Production"
@@ -15,4 +15,8 @@ export DATA_PATH="data/data_sirene3.parquet"
 export CONFIG_PATH="config/config_fasttext27.yaml"
 
 
-mlflow run ~/work/codification-ape/ --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME -P data_path=$DATA_PATH -P config_path=$CONFIG_PATH
+mlflow run ~/work/codification-ape/ --env-manager=local \
+-P remote_server_uri=$MLFLOW_TRACKING_URI \
+-P experiment_name=$MLFLOW_EXPERIMENT_NAME \
+-P data_path=$DATA_PATH \
+-P config_path=$CONFIG_PATH
