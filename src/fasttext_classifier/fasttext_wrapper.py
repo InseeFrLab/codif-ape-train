867342828036cdc0fed0c1914054671a4cf3987e
tfaria <thomas.faria@insee.fr>
2023-04-04 09:19:43 +0000

    update wrapper

--- src/fasttext_classifier/fasttext_wrapper.py
+++ src/fasttext_classifier/fasttext_wrapper.py
@@ -5,10 +5,10 @@ import sys
 
 import fasttext
 import mlflow
+import pandas as pd
 import yaml
 
-from constants import TEXT_FEATURE
-from fasttext_classifier.fasttext_evaluator import FastTextEvaluator
+from fasttext_classifier.fasttext_preprocessor import FastTextPreprocessor
 
 sys.path.append("../")
 
@@ -28,13 +28,12 @@ class FastTextWrapper(mlflow.pyfunc.PythonModel):
         """
         # pylint: disable=attribute-defined-outside-init
         self.model = fasttext.load_model(context.artifacts["fasttext_model_path"])
-        self.model_evaluator = FastTextEvaluator(self.model)
         with open(context.artifacts["config_path"], "r", encoding="utf-8") as stream:
             config = yaml.safe_load(stream)
         self.categorical_features = config["categorical_features"]
         # pylint: enable=attribute-defined-outside-init
 
-    def predict(self, context, model_input):
+    def predict(self, context, query, k):
         """
         This is an abstract function. We customized it into
         a method to fetch the FastText model.
@@ -46,23 +45,27 @@ class FastTextWrapper(mlflow.pyfunc.PythonModel):
         Returns:
             [type]: the loaded model artifact.
         """
-        # TODO: code below is duplicated from FastTextEvaluator - fix
-        # pylint: disable=no-member
-        libs = []
+        self.load_context(context)
+        preprocessor = FastTextPreprocessor()
+
+        df = preprocessor.clean_lib(df=pd.DataFrame(query), text_feature="TEXT_FEATURE")
+
+        df[self.categorical_features] = df[self.categorical_features].fillna(
+            value="NaN"
+        )
 
         iterables_features = (
             self.categorical_features if self.categorical_features is not None else []
         )
-        for item in model_input.iterrows():
-            formatted_item = item[1][TEXT_FEATURE]
+
+        libs = []
+        for item in df.iterrows():
+            formatted_item = item[1]["TEXT_FEATURE"]
             for feature in iterables_features:
                 if f"{item[1][feature]}".endswith(".0"):
                     formatted_item += f" {feature}_{item[1][feature]}"[:-2]
                 else:
                     formatted_item += f" {feature}_{item[1][feature]}"
             libs.append(formatted_item)
-            # TODO: issue with missing values ?
-        # pylint: enable=no-member
 
-        # k=1 here is temporary
-        return self.model.predict(libs, k=1)
+        return self.model.predict(libs, k=k)
