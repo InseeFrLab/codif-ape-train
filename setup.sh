85949f95e80a9479178c8fe92515e82c23921d68
ThomasFaria <thomas.faria@ensae.fr>
2024-01-29 17:36:54 +0000

    change setup

--- setup.sh
+++ setup.sh
@@ -6,12 +6,10 @@ pre-commit install
 
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
-AWS_ACCESS_KEY_ID=`vault kv get -field=ACCESS_KEY_ID onyxia-kv/projet-ape/s3` && export AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_ACCESS_KEY onyxia-kv/projet-ape/s3` && export AWS_SECRET_ACCESS_KEY
+AWS_ACCESS_KEY_ID=`vault kv get -field=ACCESS_KEY onyxia-kv/projet-ape/s3` && export AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_KEY onyxia-kv/projet-ape/s3` && export AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-
-mc cp s3/projet-ape/data/data_sirene3.parquet data/data_sirene3.parquet
 
 python - <<'END_SCRIPT'
 import nltk
