114b82ff7e657fa5006809febf2f5a5ad14f071f
ThomasFaria <thomas.faria@ensae.fr>
2024-01-30 15:53:25 +0000

    remove old file

--- bash/fetch_model_run.sh
+++
@@ -1,6 +0,0 @@
-export MLFLOW_S3_ENDPOINT_URL=https://minio.lab.sspcloud.fr
-export MLFLOW_TRACKING_URI='https://projet-ape-4549.user.lab.sspcloud.fr/'
-
-root_path="/home/onyxia/work/codification-ape"
-
-python ${root_path}/src/fetch_model.py
