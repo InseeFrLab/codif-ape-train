231f10df4afd681b51db3f55eb3615c8f06add15
ThomasFaria <thomas.faria@ensae.fr>
2024-01-31 17:30:58 +0000

    remove subsample

--- src/train.py
+++ src/train.py
@@ -188,11 +188,9 @@ def main(
         print("\n\n*** 1- Preprocessing the database...\n")
         t = time.time()
         # Load data
-        df = (
-            pq.read_table("projet-ape/extractions/20240124_sirene4.parquet", filesystem=fs)
-            .to_pandas()
-            .head(5000)
-        )
+        df = pq.read_table(
+            "projet-ape/extractions/20240124_sirene4.parquet", filesystem=fs
+        ).to_pandas()
 
         # Preprocess data
         df_train, df_test = preprocessor.preprocess(
