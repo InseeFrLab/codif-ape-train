2fca28a08df943a6211fc7ab73d7abe3af7c7bf8
tomseimandi <tom.seimandi@gmail.com>
2024-03-13 17:09:11 +0000

    Update error analysis

--- notebooks/error_analysis.py
+++ notebooks/error_analysis.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("src/")
 sys.path.append("codif-ape-train/src/")
 import hvac
@@ -14,25 +15,17 @@ from torch.utils.data import DataLoader
 
 
 # Set the environment variables for AWS
-client = hvac.Client(
-    url='https://vault.lab.sspcloud.fr', token=os.environ["VAULT_TOKEN"]
-)
+client = hvac.Client(url="https://vault.lab.sspcloud.fr", token=os.environ["VAULT_TOKEN"])
 
 secret = os.environ["VAULT_MOUNT"] + "/projet-ape/s3"
 mount_point, secret_path = secret.split("/", 1)
-secret_dict = client.secrets.kv.read_secret_version(
-    path=secret_path, mount_point=mount_point
-)
+secret_dict = client.secrets.kv.read_secret_version(path=secret_path, mount_point=mount_point)
 
-os.environ["AWS_ACCESS_KEY_ID"] = secret_dict["data"]["data"][
-    "ACCESS_KEY"
-]
-os.environ["AWS_SECRET_ACCESS_KEY"] = secret_dict["data"]["data"][
-    "SECRET_KEY"
-]
+os.environ["AWS_ACCESS_KEY_ID"] = secret_dict["data"]["data"]["ACCESS_KEY"]
+os.environ["AWS_SECRET_ACCESS_KEY"] = secret_dict["data"]["data"]["SECRET_KEY"]
 
 try:
-    del os.environ['AWS_SESSION_TOKEN']
+    del os.environ["AWS_SESSION_TOKEN"]
 except KeyError:
     pass
 
@@ -51,9 +44,7 @@ text_feature = mlflow.get_run(camembert_run_id).data.params["text_feature"]
 categorical_features = []
 for idx in range(1, 6):
     categorical_features.append(
-        mlflow.get_run(camembert_run_id).data.params.get(
-            f"categorical_features_{idx}"
-        )
+        mlflow.get_run(camembert_run_id).data.params.get(f"categorical_features_{idx}")
     )
 tokenizer_name = mlflow.get_run(camembert_run_id).data.params["pre_training_weights"]
 
@@ -64,7 +55,9 @@ df_test_ls = pd.concat(
 )
 
 df_test_ls = df_test_ls.rename(columns={text_feature: "text", Y: "labels"})
-df_test_ls["categorical_inputs"] = df_test_ls[categorical_features].apply(lambda x: x.tolist(), axis=1)
+df_test_ls["categorical_inputs"] = df_test_ls[categorical_features].apply(
+    lambda x: x.tolist(), axis=1
+)
 df_test_ls = df_test_ls.drop(columns=categorical_features)
 df_test_ls = df_test_ls[["text", "labels", "categorical_inputs"]]
 
@@ -83,9 +76,16 @@ tokenized_ds = ds.map(tokenize, batched=True, batch_size=16)
 tokenized_ds = tokenized_ds.with_format("torch")
 tokenized_ds = tokenized_ds.remove_columns("text")
 tokenized_ds = tokenized_ds.remove_columns("__index_level_0__")
-
 dataloader = DataLoader(tokenized_ds, batch_size=16)
+
+
+# Compute predictions
+def preds_from_output(output):
+    logits = output.logits
+    return logits.argmax(dim=-1)
+
+
 for batch in dataloader:
-    predictions = model(**batch)
-    print(predictions)
-    break
+    output = model(**batch)
+    labels = batch["labels"]
+    preds = preds_from_output(output)
