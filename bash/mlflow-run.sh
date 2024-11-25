c6ccc6838ebed2eed1f5c952a9964e7c98531df6
Thomas FARIA <thomas.faria@insee.fr>
2024-11-25 13:31:50 +0000

    fix entrypoint

--- bash/mlflow-run.sh
+++ bash/mlflow-run.sh
@@ -2,7 +2,7 @@
 export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr/
 export MLFLOW_S3_ENDPOINT_URL=https://minio.lab.sspcloud.fr
 export MLFLOW_EXPERIMENT_NAME='NACE2008'
-ENTRY_POINT=naf2008
+ENTRY_POINT=main
 
 # Parameters
 DIM=180
