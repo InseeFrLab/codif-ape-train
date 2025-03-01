e770b306996bc5766de22b1256f0a7406dc75afb
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-01 11:36:12 +0000

    lanchable evaluate script for label studio + test data sirene 4 with mlflow logging

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,6 +4,7 @@ import os
 import hydra
 import mlflow
 import pandas as pd
+import sklearn
 from omegaconf import DictConfig, OmegaConf
 
 from framework_classes import (
@@ -12,6 +13,9 @@ from framework_classes import (
 )
 from utils.data import get_df_naf, get_processed_data, get_test_data, get_Y
 from utils.mlflow import create_or_restore_experiment, log_dict
+from utils.validation_viz import calibration_curve, confidence_histogram, sort_and_get_pred
+
+logger = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -61,29 +65,70 @@ def evaluate(cfg: DictConfig):
     module = mlflow.pytorch.load_model(run_id)
     evaluator = EVALUATORS[cfg_dict["model"]["name"]](module)
 
-    ##### Launch evaluation #####
-    mlflow.set_experiment(exp_name + "_test")
-    with mlflow.start_run():
-        log_dict(cfg_dict)
-
+    def launch_evaluation(df, suffix, n_bins):
+        suffix = "_" + suffix
+        mlflow.log_param("num_test_observation" + suffix, len(df))
         test_res = evaluator.launch_test(
-            df_test_ls,
-            text_feature=cfg_dict["data"]["text_feature"],
-            categorical_features=cfg_dict["data"]["categorical_features"],
-            Y=Y,
-            batch_size=cfg_dict["model"]["test_params"]["test_batch_size"],
-            num_workers=os.cpu_count() - 1,
-        )[0]  # trainer.test returns a single element list
-        mlflow.log_dict(test_res, "test_results_ls")
-
-        test_res = evaluator.launch_test(
-            df_test,
+            df,
             text_feature=cfg_dict["data"]["text_feature"],
             categorical_features=cfg_dict["data"]["categorical_features"],
             Y=Y,
             batch_size=cfg_dict["model"]["test_params"]["test_batch_size"],
             num_workers=os.cpu_count() - 1,
         )[0]
-        mlflow.log_dict(test_res, "test_results_s4")
+        for key, value in test_res.items():
+            mlflow.log_metric(key + suffix, value)
+
+        predictions, inference_time = evaluator.get_preds(
+            df=df,
+            Y=Y,
+            return_inference_time=True,
+            **cfg_dict["data"],
+            batch_size=cfg_dict["model"]["test_params"]["test_batch_size"],
+            num_workers=os.cpu_count() - 1,
+        )
+        mlflow.log_metric("inference_time" + suffix, inference_time)
+
+        # Some results
+        sorted_confidence, well_predicted, predicted_confidence, predicted_class, true_values = (
+            sort_and_get_pred(predictions=predictions, df=df, Y=Y)
+        )
+        fig1 = confidence_histogram(sorted_confidence, well_predicted, df=df)
+        fig2 = calibration_curve(
+            n_bins=n_bins,
+            confidences=predicted_confidence,
+            predicted_classes=predicted_class,
+            true_labels=true_values,
+        )
+        mlflow.log_figure(fig1, "confidence_histogram" + suffix + ".png")
+        mlflow.log_figure(fig2, "calibration_curve" + suffix + ".png")
+
+        brier_score = sklearn.metrics.brier_score_loss(
+            well_predicted, predicted_confidence.numpy(), sample_weight=None, pos_label=1
+        )
+        mlflow.log_metric("brier_score" + suffix, brier_score)
+
+        return predictions
+
+    ##### Launch evaluation #####
+    mlflow.set_experiment(exp_name + "_test")
+    with mlflow.start_run():
+        log_dict(cfg_dict)
+
+        ### Label Studio test data ###
+        logger.info("Launching evaluation on Label Studio test data.")
+        predictions = launch_evaluation(df_test_ls, suffix="ls", n_bins=40)
+
+        # Predictions in readable format - with true NACE code
+        df_res = evaluator.get_aggregated_preds(df=df_test_ls, predictions=predictions, Y=Y)
+        mlflow.log_table(df_res, "predictions.json")
+
+        ### Sirene 4 test data ###
+        logger.info("Launching evaluation on Sirene 4 test data.")
+        predictions = launch_evaluation(df_test, suffix="s4", n_bins=100)
 
     return
+
+
+if __name__ == "__main__":
+    evaluate()
