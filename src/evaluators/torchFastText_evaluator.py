10564b16b36fd04ab8e61a5648112bcd30bb8b9e
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-01 08:39:49 +0000

    doc strings

--- src/evaluators/torchFastText_evaluator.py
+++ src/evaluators/torchFastText_evaluator.py
@@ -24,7 +24,8 @@ class torchFastTextEvaluator(FastTextModule, Evaluator):
 
     def test_step(self, batch, batch_idx):
         """
-        Designed for
+        Designed for evluation on a labeled set, inference AND computation of some metrics.
+        To be called with self.trainer.test(self, dataloader) - see launch_test method.
 
         """
         start = time.time()
@@ -58,6 +59,10 @@ class torchFastTextEvaluator(FastTextModule, Evaluator):
         return loss, accuracy
 
     def predict_step(self, batch, batch_idx):
+        """
+        Same as test_step but without the loss and accuracy computation: boilerplate inference.
+        To be called with self.trainer.predict(self, dataloader) - see get_preds method.
+        """
         inputs, _ = batch[:-1], batch[-1]
         outputs = self.forward(inputs)
         return outputs
@@ -85,6 +90,30 @@ class torchFastTextEvaluator(FastTextModule, Evaluator):
     def get_preds(
         self, df, text_feature, categorical_features, Y, batch_size, num_workers, k=None, **kwargs
     ):
+        """
+        Returns the prediction of the model for pd.DataFrame `df`
+        along with the output probabilities if applicable.
+
+        Args:
+            df (pd.DataFrame): Evaluation DataFrame, containing processed text and "tokenized" categorical features.
+            text_feature (str): Name of the text feature.
+            categorical_features (List[str]): Names of the categorical features.
+            Y (str): Name of the variable to predict.
+            batch_size (int): Batch size for the evaluation.
+            num_workers (int): Number of workers for the evaluation.
+            k (int): Number of predictions.
+                If k is None, return the probabilities of each class.
+                If k is not None, return the top k predictions and their probabilities.
+
+        Returns:
+            If k is None:
+                predictions (torch.Tensor): Tensor of shape (n_samples, n_classes) containing the probabilities of each class.
+            If k is not None:
+                preds (np.ndarray): Array of shape (n_samples, k) containing the top k indices (int).
+                probs (np.ndarray): Array of shape (n_samples, k) containing the probabilities of the top k predictions.
+
+
+        """
         text, categorical_variables = (
             df[text_feature].values,
             df[categorical_features].values,
