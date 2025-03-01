ab67aedfa535acdab30a0ea17d806850d2667d45
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-01 12:24:53 +0000

    update to include fastText

--- src/framework_classes.py
+++ src/framework_classes.py
@@ -6,18 +6,16 @@ import torch
 from torchFastText.datasets import FastTextModelDataset, NGramTokenizer
 from torchFastText.model import FastTextModel, FastTextModule
 
-from evaluators import torchFastTextEvaluator
-from preprocessors.pytorch_preprocessor import PytorchPreprocessor
+from evaluators import FastTextEvaluator, torchFastTextEvaluator
+from models.fasttext_classifier.fasttext_wrapper import FastTextWrapper
+from preprocessors import FastTextPreprocessor, PytorchPreprocessor
 from trainers.build_trainers import (
     build_lightning_trainer,
     build_transformers_trainer,
 )
+from trainers.fasttext_trainer import FastTextTrainer
 from utils.data import get_all_data, get_sirene_3_data, get_sirene_4_data
 
-# from fasttext_classifier.fasttext_evaluator import FastTextEvaluator
-# from fasttext_classifier.fasttext_preprocessor import FastTextPreprocessor
-# from fasttext_classifier.fasttext_trainer import FastTextTrainer
-# from fasttext_classifier.fasttext_wrapper import FastTextWrapper
 # from pytorch_classifiers.models.camembert.camembert_model import (
 #     CustomCamembertModel,
 #     EmbeddedCategoricalCamembertModel,
@@ -29,7 +27,7 @@ from utils.data import get_all_data, get_sirene_3_data, get_sirene_4_data
 #     OneHotCategoricalCamembertWrapper,
 # )
 
-PREPROCESSORS = {"PyTorch": PytorchPreprocessor}
+PREPROCESSORS = {"PyTorch": PytorchPreprocessor, "fastText": FastTextPreprocessor}
 DATA_GETTER = {
     "sirene_3": get_sirene_3_data,
     "sirene_4": get_sirene_4_data,
@@ -37,7 +35,7 @@ DATA_GETTER = {
 }
 TOKENIZERS = {"NGramTokenizer": NGramTokenizer}
 DATASETS = {"FastTextModelDataset": FastTextModelDataset}
-MODELS = {"torchFastText": FastTextModel}
+MODELS = {"torchFastText": FastTextModel, "fastText": FastTextWrapper}
 MODULES = {"torchFastText": FastTextModule}
 OPTIMIZERS = {
     "Adam": torch.optim.Adam,
@@ -46,5 +44,9 @@ OPTIMIZERS = {
 }
 SCHEDULERS = {"ReduceLROnPlateau": torch.optim.lr_scheduler.ReduceLROnPlateau}
 LOSSES = {"CrossEntropyLoss": torch.nn.CrossEntropyLoss}
-TRAINERS = {"Lightning": build_lightning_trainer, "Transformers": build_transformers_trainer}
-EVALUATORS = {"torchFastText": torchFastTextEvaluator}
+TRAINERS = {
+    "Lightning": build_lightning_trainer,
+    "Transformers": build_transformers_trainer,
+    "fastText": FastTextTrainer,
+}
+EVALUATORS = {"torchFastText": torchFastTextEvaluator, "fastText": FastTextEvaluator}
