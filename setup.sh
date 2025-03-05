2818d94428585e32d2cc11793736bb05ee735dca
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-05 08:25:36 +0000

    add mlflow_tracking_uri

--- setup.sh
+++ setup.sh
@@ -16,4 +16,6 @@ AWS_SECRET_ACCESS_KEY=`vault kv get -field=AWS_SECRET_ACCESS_KEY onyxia-kv/proje
 unset AWS_SESSION_TOKEN
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
 
+export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr/
+
 python -m nltk.downloader stopwords
