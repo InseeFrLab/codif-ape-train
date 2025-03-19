e04cb06af7c990ea6b3f0e504e809edc0969c73c
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-19 11:54:09 +0000

    improve : no hardcoding of the file names but dynamic retrieval of the jsons that are present in the mapping folder

--- src/utils/mappings.py
+++ src/utils/mappings.py
@@ -1,12 +1,26 @@
 """
 Mappings of categorical variables.
+
+The jsons in data/mappings should be named accordingly:
+    XXX_suffix
+where XXX is the name of the variable, and suffix is common for all mappings.
+
+As for now, the suffix is "_mapping.json".
+
 """
 
 import json
+import os
 
 mappings = {}
-for key in ["apet_finale", "CJ", "CRT", "NAT", "TYP"]:
-    file = f"data/mappings/{key}_mapping.json"
+mapping_dir = "data/mappings"
+suffix = "_mapping.json"
+
+# dynamically fetch the mapping jsons in the mapping directory
+keys = [f[: -len(suffix)] for f in os.listdir(mapping_dir) if f.endswith(suffix)]
+
+for key in keys:
+    file = os.path.join(mapping_dir, key + suffix)
     with open(file, "r") as f:
         mappings[key] = json.load(f)
 
