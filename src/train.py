1437a89fb2abbd1dc7c42f31ba120d0f2c8274f2
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-19 11:45:33 +0000

    fix : removed load_or_preprocess to put it in preprocess.py. More clarity

--- src/train.py
+++ src/train.py
@@ -4,23 +4,20 @@ import sys
 
 import hydra
 import mlflow
-import pandas as pd
 import torch
 from omegaconf import DictConfig, OmegaConf
 
 from framework_classes import (
-    DATA_GETTER,
     DATASETS,
     LOSSES,
     MODELS,
     MODULES,
     OPTIMIZERS,
-    PREPROCESSORS,
     SCHEDULERS,
     TOKENIZERS,
     TRAINERS,
 )
-from utils.data import get_df_naf, get_processed_data, get_Y
+from utils.data import get_processed_data, get_Y
 from utils.mappings import mappings
 from utils.mlflow import create_or_restore_experiment, log_dict
 
@@ -34,57 +31,6 @@ logging.basicConfig(
 )
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-def load_or_preprocess_data(cfg_dict_data, cfg_dict_model_preprocessor):
-    """
-    Load and preprocess data, using joblib caching to avoid redundant computation.
-    """
-    # Fetch data
-    df_s3, df_s4 = DATA_GETTER[cfg_dict_data["sirene"]](**cfg_dict_data)
-    Y = get_Y(revision=cfg_dict_data["revision"])
-    df_naf = get_df_naf(revision=cfg_dict_data["revision"])
-
-    # Preprocess data
-    preprocessor = PREPROCESSORS[cfg_dict_model_preprocessor]()
-
-    if df_s4 is not None:
-        df_train_s4, df_val_s4, df_test = preprocessor.preprocess(
-            df=df_s4,
-            df_naf=df_naf,
-            y=Y,
-            text_feature=cfg_dict_data["text_feature"],
-            textual_features=cfg_dict_data["textual_features"],
-            categorical_features=cfg_dict_data["categorical_features"],
-            test_size=0.1,
-        )
-    else:
-        raise ValueError("Sirene 4 data should be provided.")
-
-    if df_s3 is not None:
-        df_train_s3, df_val_s3, df_test_s3 = preprocessor.preprocess(
-            df=df_s3,
-            df_naf=df_naf,
-            y=Y,
-            text_feature=cfg_dict_data["text_feature"],
-            textual_features=cfg_dict_data["textual_features"],
-            categorical_features=cfg_dict_data["categorical_features"],
-            test_size=0.1,
-            s3=True,
-        )
-        # Merge Sirene 3 into the training set
-        df_s3_processed = pd.concat([df_train_s3, df_val_s3, df_test_s3])
-        df_train = pd.concat([df_s3_processed, df_train_s4]).reset_index(drop=True)
-
-        # Assert no data was lost
-        assert len(df_s3) == len(df_s3_processed)
-        assert len(df_train_s4) + len(df_s3) == len(df_train)
-
-    else:
-        df_train = df_train_s4
-
-    df_val = df_val_s4
-    return df_train, df_val, df_test, Y
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
