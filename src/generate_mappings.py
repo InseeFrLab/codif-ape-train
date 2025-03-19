75b9a99dc75173fa7a0c57a2bd9a06c623a8dad2
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-19 11:41:51 +0000

    dev : generate the Y mapping for nace2025, adapting the script too.

--- src/generate_mappings.py
+++ src/generate_mappings.py
@@ -17,8 +17,6 @@ It generates the json files in data/mappings.
 
 """
 
-
-revision = "NAF2008"  # not impactful
 text_feature = "libelle"
 textual_features_1 = "NAT_LIB"
 textual_features_2 = "AGRI"
@@ -29,31 +27,9 @@ categorical_features_4 = "CJ"
 categorical_features_5 = "CRT"
 
 
-def generate_mappings(start_month=1, start_year=2018):
+def generate_mappings(start_month=1, start_year=2018, revision="NAF2008"):
     Y = get_Y(revision=revision)
-    params = {
-        key: value
-        for key, value in locals().items()
-        if (
-            key
-            not in [
-                "remote_server_uri",
-                "experiment_name",
-                "run_name",
-                "revision",
-                "Y",
-                "model_class",
-                "text_feature",
-                "pre_training_weights",
-                "start_month",
-                "start_year",
-            ]
-        )
-        and not key.startswith("textual_features")
-        and not key.startswith("categorical_features")
-        and not key.startswith("embedding_dim")
-    }
-    params["thread"] = os.cpu_count()
+
     textual_features = [
         value for key, value in globals().items() if key.startswith("textual_features_")
     ]
@@ -61,10 +37,30 @@ def generate_mappings(start_month=1, start_year=2018):
         value for key, value in globals().items() if key.startswith("categorical_features_")
     ]
     # Load data
+
     # Sirene 4
-    df_s3, df_s4 = get_all_data(revision=revision, start_month=start_month, start_year=start_year)
+
+    if revision == "NAF2008":
+        df_s3, df_s4 = get_all_data(
+            revision=revision, start_month=start_month, start_year=start_year
+        )
+
     # Detailed NAF
     df_naf = get_df_naf(revision=revision)
+
+    directory = "data/mappings/"
+    os.makedirs(directory, exist_ok=True)  # Ensure the directory exists
+
+    # Y mapping from df_naf
+    sorted_by_count_unique_values = df_naf["APE_NIV5"].value_counts()
+    mapping = {k: v for v, k in enumerate(sorted_by_count_unique_values.index)}
+
+    with open(os.path.join(directory, f"{Y}_mapping.json"), "w") as f:
+        json.dump(mapping, f, indent=3)
+
+    # for naf2025, only generate Y mapping and exit
+    if revision == "NAF2025":
+        return
 
     preprocessor = PytorchPreprocessor()
 
@@ -92,9 +88,8 @@ def generate_mappings(start_month=1, start_year=2018):
     )
 
     df = pd.concat([df_s3, df_s4], axis=0).reset_index(drop=True)
-    directory = "data/mappings/"
-    os.makedirs(directory, exist_ok=True)  # Ensure the directory exists
-    for variable in tqdm(categorical_features + [Y]):
+
+    for variable in tqdm(categorical_features):
         if variable != "SRF":
             sorted_by_count_unique_values = df[variable].value_counts()
             mapping = {k: v for v, k in enumerate(sorted_by_count_unique_values.index)}
@@ -111,6 +106,9 @@ if __name__ == "__main__":
 
     parser.add_argument("--start_month", type=int, default=1)
     parser.add_argument("--start_year", type=int, default=2018)
+    parser.add_argument("--revision", type=str, default="NAF2008")
 
     args = parser.parse_args()
-    generate_mappings(start_month=args.start_month, start_year=args.start_year)
+    generate_mappings(
+        start_month=args.start_month, start_year=args.start_year, revision=args.revision
+    )
