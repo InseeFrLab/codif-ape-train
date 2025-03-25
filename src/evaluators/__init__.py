77e98c025cfe329cf8c1b01d8ef0d4003328749e
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-25 13:00:11 +0000

    fix : import abstract class for get_aggregated_preds

--- src/evaluators/__init__.py
+++ src/evaluators/__init__.py
@@ -1,2 +1,3 @@
+from .base.evaluator import Evaluator as Evaluator
 from .fasttext_evaluator import FastTextEvaluator as FastTextEvaluator
 from .torchFastText_evaluator import torchFastTextEvaluator as torchFastTextEvaluator
