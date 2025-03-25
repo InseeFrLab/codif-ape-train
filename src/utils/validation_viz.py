150eb1decbfe0a5974d9e272967dff9cec259dbf
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-03-25 14:57:50 +0000

    feature - wip : add comparisons between torch and model in production.
    Next step: quarto reproducible webpage

--- src/utils/validation_viz.py
+++ src/utils/validation_viz.py
@@ -64,3 +64,33 @@ def calibration_curve(n_bins, confidences, predicted_classes, true_labels):
     plt.legend()
     plt.grid()
     return fig
+
+
+def get_automatic_accuracy(thresholds, predicted_confidence, predicted_class, true_values):
+    """
+    We automatically code the APE if the confidence is above the threshold.
+    Compute the accuracy on the automatically coded APEs.
+
+    Args:
+        thresholds (np.array(float), shape (n_thresholds,)): The threshold for automatic coding.
+        predicted_confidence (torch.Tensor, shape (n_samples, 1): The confidence of the predictions.
+        predicted_class (torch.Tensor, shape (n_samples, 1)
+            The predicted class of the APEs.
+        true_values (torch.Tensor, shape (n_samples, 1): The true values of the APEs.
+
+    Returns:
+        automatic_coding_rate (float): The rate of automatically coded APEs.
+        accuracy_automatic (float): The accuracy on the automatically coded APEs.
+
+    """
+    n_thresholds = len(thresholds)
+    automatic_coding_mask = predicted_confidence[:, None] >= thresholds[None, :]
+    automatic_coding_rate = automatic_coding_mask.mean(axis=0)
+
+    predicted_class_expanded = np.repeat(predicted_class[:, None], n_thresholds, 1)
+    true_values_expanded = np.repeat(true_values[:, None], n_thresholds, 1)
+
+    predicted_automatic = np.ma.array(predicted_class_expanded, mask=~automatic_coding_mask)
+    ground_truth_automatic = np.ma.array(true_values_expanded, mask=~automatic_coding_mask)
+    accuracy_automatic = (predicted_automatic == ground_truth_automatic).mean(axis=0)
+    return automatic_coding_rate, accuracy_automatic
