fe222d9ac2ed52fdee64494dfd45184ac931f23b
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-04-08 15:34:24 +0000

    [chore] dependency and setup
    Add notebook dependencies
    nltk downloader from uv and in setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git submodule update --init
 
 pip install uv
 uv sync
+uv run -m nltk.downloader stopwords
 pre-commit install
 
 export PYTHONPATH="$PWD:$PYTHONPATH"
@@ -15,5 +16,3 @@ unset AWS_SESSION_TOKEN
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
 
 export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr/
-
-python -m nltk.downloader stopwords
