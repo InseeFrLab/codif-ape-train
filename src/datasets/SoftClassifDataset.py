168a7998ac93c985eff6a3438937af515a24379b
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-04-15 16:44:03 +0000

    [feature] Add SoftClassif dataset
    - Refactored
    - train.py adaptation
    - config files

---
+++ src/datasets/SoftClassifDataset.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+import torch
+from torchFastText.datasets import FastTextModelDataset
+
+from utils.data import get_df_naf, get_Y
+from utils.mappings import mappings
+
+
+class SoftClassifDataset(FastTextModelDataset):
+    """
+    SoftClassifDataset is a subclass of FastTextModelDataset that is designed for soft classification tasks.
+    It allows for the use of soft labels: the ground truth is not a single class, but all classes can have a probability to be true (in [0,1]).
+    """
+
+    def __init__(self, similarity_coefficients, revision, *args, **kwargs):
+        super(SoftClassifDataset, self).__init__(*args, **kwargs)
+
+        assert len(similarity_coefficients) == 5  # 5 levels
+        assert revision == "NAF2008" or revision == "NAF2025"
+        self.similarity_coefficients = similarity_coefficients
+        self.revision = revision
+        self.similarity_matrix = self.generate_similarity_matrix()
+
+    def generate_similarity_matrix(self):
+        df = get_df_naf(self.revision)
+        Y = get_Y(revision=self.revision)
+        levels_matrix = df[["APE_NIV1", "APE_NIV2", "APE_NIV3", "APE_NIV4", "APE_NIV5"]].values
+        similarity_matrix = np.dot(
+            (levels_matrix[:, None, :] == levels_matrix[None, :, :]), self.similarity_coefficients
+        )
+        similarity_df = pd.DataFrame(
+            similarity_matrix, index=df["APE_NIV5"], columns=df["APE_NIV5"]
+        )
+        ordered_similarity_df = similarity_df.loc[mappings[Y].keys(), mappings[Y].keys()]
+        similarity_matrix = ordered_similarity_df.values
+        return similarity_matrix
+
+    def collate_fn(self, batch):
+        """
+        Efficient batch processing without explicit loops.
+
+        Args:
+            batch: Data batch.
+
+        Returns:
+            Tuple[torch.LongTensor]: Observation with given index.
+        """
+
+        # Unzip the batch in one go using zip(*batch)
+        if self.outputs is not None:
+            text, *categorical_vars, y = zip(*batch)
+        else:
+            text, *categorical_vars = zip(*batch)
+
+        # Convert text to indices in parallel using map
+        indices_batch = list(map(lambda x: self.tokenizer.indices_matrix(x)[0], text))
+
+        # Get padding index once
+        padding_index = self.tokenizer.get_buckets() + self.tokenizer.get_nwords()
+
+        # Pad sequences efficiently
+        padded_batch = torch.nn.utils.rnn.pad_sequence(
+            indices_batch,
+            batch_first=True,
+            padding_value=padding_index,
+        )
+
+        # Handle categorical variables efficiently
+        if self.categorical_variables is not None:
+            categorical_tensors = torch.stack(
+                [
+                    torch.tensor(cat_var, dtype=torch.float32)
+                    for cat_var in categorical_vars[
+                        0
+                    ]  # Access first element since zip returns tuple
+                ]
+            )
+        else:
+            categorical_tensors = torch.empty(
+                padded_batch.shape[0], 1, dtype=torch.float32, device=padded_batch.device
+            )
+
+        if self.outputs is not None:
+            y = self.similarity_matrix[list(y), :]  # (batch_size, n_classes)
+            y = torch.tensor(y, dtype=torch.float)
+            return (padded_batch, categorical_tensors, y)
+        else:
+            return (padded_batch, categorical_tensors)
--- src/train.py
+++ src/train.py
@@ -4,9 +4,14 @@ import sys
 
 import hydra
 import mlflow
+import numpy as np
+import pandas as pd
+import sklearn
 import torch
 from omegaconf import DictConfig, OmegaConf
+from torchFastText.datasets import FastTextModelDataset
 
+from evaluators import Evaluator
 from framework_classes import (
     DATASETS,
     LOSSES,
@@ -17,9 +22,16 @@ from framework_classes import (
     TOKENIZERS,
     TRAINERS,
 )
-from utils.data import get_processed_data, get_Y
+from utils.data import PATHS, get_file_system, get_processed_data, get_Y
 from utils.mappings import mappings
 from utils.mlflow import create_or_restore_experiment, log_dict
+from utils.validation_viz import (
+    calibration_curve,
+    confidence_histogram,
+    get_automatic_accuracy,
+    plot_automatic_coding_accuracy_curve,
+    sort_and_get_pred,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -48,6 +60,10 @@ def train(cfg: DictConfig):
         Y = get_Y(revision=cfg_dict["data"]["revision"])
         df_train, df_val, df_test = get_processed_data(revision=cfg_dict["data"]["revision"])
 
+        df_train = df_train.sample(frac=0.001)
+        df_val = df_val.sample(frac=0.01)
+        df_test = df_test.sample(frac=0.01)
+
         mlflow.log_param("number_of_training_observations", df_train.shape[0])
 
         train_text, train_categorical_variables = (
@@ -57,6 +73,10 @@ def train(cfg: DictConfig):
         val_text, val_categorical_variables = (
             df_val[cfg_dict["data"]["text_feature"]].values,
             df_val[cfg_dict["data"]["categorical_features"]].values,
+        )
+        test_text, test_categorical_variables = (
+            df_test[cfg_dict["data"]["text_feature"]].values,
+            df_test[cfg_dict["data"]["categorical_features"]].values,
         )
 
         ###### Tokenizer ######
@@ -68,27 +88,45 @@ def train(cfg: DictConfig):
 
         ###### Dataset ######
 
-        if cfg_dict["model"]["dataset"] is not None:
-            dataset_class = DATASETS[cfg_dict["model"]["dataset"]]
+        if cfg_dict["dataset"] is not None:
+            similarity_coefficients = cfg_dict["dataset"].get("similarity_coefficients", None)
+
+            dataset_class = DATASETS[cfg_dict["dataset"]["dataset_name"]]
 
             train_dataset = dataset_class(
                 texts=train_text,
                 categorical_variables=train_categorical_variables,
                 tokenizer=tokenizer,
                 outputs=df_train[Y].values,
+                revision=cfg_dict["data"]["revision"],
+                similarity_coefficients=similarity_coefficients,
             )
             val_dataset = dataset_class(
                 texts=val_text,
                 categorical_variables=val_categorical_variables,
                 tokenizer=tokenizer,
                 outputs=df_val[Y].values,
+                revision=cfg_dict["data"]["revision"],
+                similarity_coefficients=similarity_coefficients,
             )
 
-            if cfg_dict["model"]["dataset"] == "FastTextModelDataset":
+            test_dataset = dataset_class(
+                texts=test_text,
+                categorical_variables=test_categorical_variables,
+                tokenizer=tokenizer,
+                outputs=df_test[Y].values,
+                revision=cfg_dict["data"]["revision"],
+                similarity_coefficients=similarity_coefficients,
+            )
+
+            if isinstance(train_dataset, FastTextModelDataset):
                 train_dataloader = train_dataset.create_dataloader(
                     **cfg_dict["model"]["train_params"]
                 )
                 val_dataloader = val_dataset.create_dataloader(**cfg_dict["model"]["train_params"])
+                test_dataloader = test_dataset.create_dataloader(
+                    **cfg_dict["model"]["train_params"]
+                )
 
         ###### Model #####
         num_classes = max(mappings[Y].values()) + 1
@@ -103,14 +141,14 @@ def train(cfg: DictConfig):
         logger.info("Number of classes: " + str(num_classes))
         logger.info("categorical_vocab_sizes " + str(categorical_vocab_sizes))
 
-        if cfg_dict["model"]["name"] == "torchFastText":
+        if cfg_dict["model"]["model_name"] == "torchFastText":
             # for torchFastText only, we add the number of words in the vocabulary
             # In general, tokenizer.num_tokens == num_rows is a invariant
             num_rows = tokenizer.num_tokens + tokenizer.get_nwords() + 1
             padding_idx = num_rows - 1
 
         # PyTorch model
-        model = MODELS[cfg_dict["model"]["name"]](
+        model = MODELS[cfg_dict["model"]["model_name"]](
             **cfg_dict["model"]["model_params"],
             tokenizer=tokenizer,
             num_rows=num_rows,
@@ -129,7 +167,7 @@ def train(cfg: DictConfig):
         ]  # without the () !
         scheduler = SCHEDULERS[cfg_dict["model"]["train_params"]["scheduler_name"]]
 
-        module = MODULES[cfg_dict["model"]["name"]](
+        module = MODULES[cfg_dict["model"]["model_name"]](
             model=model,
             loss=loss,
             optimizer=optimizer,
@@ -144,7 +182,6 @@ def train(cfg: DictConfig):
         ###### Trainer #####
         trainer = TRAINERS[cfg_dict["model"]["train_params"]["trainer_name"]](
             **cfg_dict["model"]["train_params"],
-            experiment_name=cfg_dict["mlflow"]["experiment_name"],
         )
 
         if cfg_dict["model"]["preprocessor"] == "PyTorch":
@@ -155,8 +192,80 @@ def train(cfg: DictConfig):
         trainer.fit(module, train_dataloader, val_dataloader)
 
         ########## Evaluation ##########
+        logger.info("Starting evaluation...")
+        fs = get_file_system()
+        df_res_ft = pd.read_parquet(
+            PATHS[cfg_dict["data"]["revision"]][-1][:-8] + "_predictions_ft.parquet", filesystem=fs
+        )
+        fasttext_preds_labels = df_res_ft["APE_NIV5_pred_k1"].values
+        fasttext_preds_scores = df_res_ft["proba_k1"].values
+        ground_truth = df_res_ft["APE_NIV5"]
 
-        # To do
+        def run_eval(df, dataloader, suffix="val", n_bins=100):
+            """
+            Run evaluation on the given dataloader and log the results.
+            """
+
+            predictions = trainer.predict(
+                module, dataloader
+            )  # accumulates predictions over batches
+            predictions_tensor = torch.cat(predictions).cpu()  # (num_test_samples, num_classes)
+
+            (
+                sorted_confidence,
+                well_predicted,
+                predicted_confidence,
+                predicted_class,
+                true_values,
+            ) = sort_and_get_pred(predictions=predictions_tensor, df=df, Y=Y)
+            fig1 = confidence_histogram(sorted_confidence, well_predicted, df=df)
+            fig2 = calibration_curve(
+                n_bins=n_bins,
+                confidences=predicted_confidence,
+                predicted_classes=predicted_class,
+                true_labels=true_values,
+            )
+            mlflow.log_figure(fig1, "confidence_histogram_" + suffix + ".png")
+            mlflow.log_figure(fig2, "calibration_curve_" + suffix + ".png")
+
+            brier_score = sklearn.metrics.brier_score_loss(
+                well_predicted, predicted_confidence.numpy(), sample_weight=None, pos_label=1
+            )
+            mlflow.log_metric("brier_score" + suffix, brier_score)
+
+            # Use your aggregation function
+            aggregated_results = Evaluator.get_aggregated_preds(
+                df=df, Y=Y, predictions=predictions_tensor.numpy(), top_k=1
+            )
+
+            accuracy = Evaluator.compute_accuracies(
+                aggregated_preds=aggregated_results, suffix=suffix
+            )
+            mlflow.log_metrics(accuracy)
+
+            if suffix == "test":
+                thresholds = np.linspace(0, 1, 100)
+                torchft_scores = sorted_confidence[:, 0] - sorted_confidence[:, 1:5].sum(axis=1)
+                torchft_plot = get_automatic_accuracy(
+                    thresholds,
+                    torch.clamp(torchft_scores, 0, 1).numpy(),
+                    predicted_class.numpy(),
+                    true_values,
+                )
+                ft_plot = get_automatic_accuracy(
+                    thresholds,
+                    np.clip(fasttext_preds_scores.reshape(-1), 0, 1),
+                    fasttext_preds_labels.reshape(-1),
+                    ground_truth.values,
+                )
+
+                fig = plot_automatic_coding_accuracy_curve(torchft_plot, ft_plot, thresholds)
+                mlflow.log_figure(fig, "automatic_coding_accuracy_curve.png")
+
+            return
+
+        run_eval(df_val, val_dataloader, suffix="val")
+        run_eval(df_test, test_dataloader, suffix="test")
 
 
 if __name__ == "__main__":
