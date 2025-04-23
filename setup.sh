b722bde6aa69fe4f148ddf16428df5a3c148fc5d
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-04-23 14:30:24 +0000

    [chore] dependency + small fix env variable
    Add torch uncertainty

--- setup.sh
+++ setup.sh
@@ -6,9 +6,7 @@ uv run pre-commit install
 
 export PYTHONPATH="$PWD:$PYTHONPATH"
 
-AWS_ACCESS_KEY_ID=`vault kv get -field=AWS_ACCESS_KEY_ID onyxia-kv/projet-ape/s3` && export AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY=`vault kv get -field=AWS_SECRET_ACCESS_KEY onyxia-kv/projet-ape/s3` && export AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
+export AWS_SESSION_TOKEN=""
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
 
 export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr/
