63e800bd833b99c6ad8664df74d343b22f8466a4
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-04-30 17:55:15 +0000

    [fix] add revision parameter where it was necessary

--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -59,89 +59,6 @@ def run_evaluation(trainer, module, revision, Y, zipped_data):
 ######### Utils for the run_evaluation method ##########
 
 
-def get_fasttext_preds(revision):
-    fs = get_file_system()
-    df_res_ft = pd.read_parquet(PATHS[revision][-1][:-8] + "_predictions_ft.parquet", filesystem=fs)
-    fasttext_preds_labels = df_res_ft[["APE_NIV5_pred_k1"]]
-    fasttext_preds_scores = df_res_ft[["proba_k1"]]
-
-    return fasttext_preds_labels, fasttext_preds_scores
-
-
-def get_inv_mapping(revision):
-    assert revision in [
-        "NAF2008",
-        "NAF2025",
-    ], f"Invalid revision: {revision} - must be NAF2008 or NAF2025."
-    if revision == "NAF2008":
-        key = "apet_finale"
-    else:
-        key = "nace2025"
-
-    ape_niv5_mapping = mappings[key]
-    inv_ape_niv5_mapping = {v: k for k, v in ape_niv5_mapping.items()}
-
-    return inv_ape_niv5_mapping
-
-
-def get_ground_truth(df, Y):
-    true_values = df[Y].values
-    return true_values
-
-
-def get_well_predicted_mask(predicted_class, true_values):
-    well_predicted = (predicted_class == true_values).float()
-    return well_predicted
-
-
-def sort_and_get_pred(predictions, true_values):
-    sorted_confidence, sorted_confidence_indices = predictions.sort(descending=True)
-
-    predicted_class = sorted_confidence_indices[:, 0]
-    predicted_confidence = sorted_confidence[:, 0]
-
-    return sorted_confidence, predicted_confidence, predicted_class
-
-
-def get_all_levels(df, df_naf, col):
-    """
-    Get all levels of the NAF code from the dataframe.
-
-    Args:
-        df (pd.DataFrame): DataFrame containing the NAF codes.
-        df_naf (pd.DataFrame): DataFrame containing the NAF data.
-
-    Returns:
-        pd.DataFrame: DataFrame with all levels of the NAF code.
-    """
-    df = df[[col]]
-
-    if df[col].dtype == "int64":
-        inv_ape_niv5_mapping = get_inv_mapping("NAF2025")
-        df[col] = df[col].map(inv_ape_niv5_mapping)
-
-    df_full = df.merge(df_naf, left_on=col, right_on="APE_NIV5").drop(columns=[col, "LIB_NIV5"])
-
-    return df_full
-
-
-def get_pred_nafs(predicted_class):
-    """
-    Get the predicted NAF codes from the predicted class.
-
-    Args:
-        predicted_class (torch.Tensor): Predicted class tensor.
-
-    Returns:
-        pd.DataFrame: DataFrame with the predicted NAF codes.
-    """
-    inv_ape_niv5_mapping = get_inv_mapping("NAF2025")
-    df_pred = pd.DataFrame(predicted_class.numpy(), columns=["APE_NIV5_pred"])
-    df_pred["APE_NIV5_pred"] = df_pred["APE_NIV5_pred"].map(inv_ape_niv5_mapping)
-
-    return df_pred
-
-
 def run_eval(
     df,
     df_naf,
@@ -174,9 +91,9 @@ def run_eval(
     mlflow.log_metric("brier_score" + suffix, brier_score)
 
     # Get and log accuracy at all levels
-    naf_predictions = get_pred_nafs(predicted_class)
-    all_level_preds = get_all_levels(naf_predictions, df_naf, "APE_NIV5_pred")
-    all_level_ground_truth = get_all_levels(df, df_naf, Y)
+    naf_predictions = get_pred_nafs(predicted_class, revision=revision)
+    all_level_preds = get_all_levels(naf_predictions, df_naf)
+    all_level_ground_truth = get_all_levels(df, df_naf, col=Y, revision=revision)
 
     accs = (all_level_preds == all_level_ground_truth).mean(axis=0)
 
@@ -203,3 +120,129 @@ def run_eval(
         mlflow.log_figure(fig, "automatic_coding_accuracy_curve.png")
 
     return
+
+
+def get_fasttext_preds(revision):
+    """
+    Get the fastText (production API) predictions for the given revision.
+
+    Args:
+        revision (str): The revision of the NAF code.
+            Must be either "NAF2008" or "NAF2025".
+    Returns:
+        fasttext_preds_labels (pd.DataFrame): DataFrame with the predicted labels, tokenized (int)
+        fasttext_preds_scores (pd.DataFrame): DataFrame with the predicted scores (float).
+
+
+    """
+    fs = get_file_system()
+    df_res_ft = pd.read_parquet(PATHS[revision][-1][:-8] + "_predictions_ft.parquet", filesystem=fs)
+    mapping = get_label_mapping(revision)
+
+    fasttext_preds_labels = df_res_ft[["APE_NIV5_pred_k1"]]
+    fasttext_preds_scores = df_res_ft[["proba_k1"]]
+    fasttext_preds_labels_int = fasttext_preds_labels.map(mapping.get)
+
+    return fasttext_preds_labels_int, fasttext_preds_scores
+
+
+def get_label_mapping(revision):
+    """
+    Get the label mapping for the given revision.
+
+    Args:
+        revision (str): The revision of the NAF code.
+            Must be either "NAF2008" or "NAF2025".
+    Returns:
+        dict[str] -> int : The label mapping for the given revision.
+
+    """
+    assert revision in [
+        "NAF2008",
+        "NAF2025",
+    ], f"Invalid revision: {revision} - must be NAF2008 or NAF2025."
+    if revision == "NAF2008":
+        key = "apet_finale"
+    else:
+        key = "nace2025"
+
+    ape_niv5_mapping = mappings[key]
+
+    return ape_niv5_mapping
+
+
+def get_inv_mapping(revision):
+    """
+    Get the inverse label mapping for the given revision.
+    Args:
+        revision (str): The revision of the NAF code.
+            Must be either "NAF2008" or "NAF2025".
+    Returns:
+        dict[int] -> str : The inverse label mapping for the given revision.
+
+    """
+
+    ape_niv5_mapping = get_label_mapping(revision)
+    inv_ape_niv5_mapping = {v: k for k, v in ape_niv5_mapping.items()}
+
+    return inv_ape_niv5_mapping
+
+
+def get_ground_truth(df, Y):
+    true_values = df[Y].values
+    return true_values
+
+
+def get_well_predicted_mask(predicted_class, true_values):
+    well_predicted = (predicted_class == true_values).float()
+    return well_predicted
+
+
+def sort_and_get_pred(predictions, true_values):
+    sorted_confidence, sorted_confidence_indices = predictions.sort(descending=True)
+
+    predicted_class = sorted_confidence_indices[:, 0]
+    predicted_confidence = sorted_confidence[:, 0]
+
+    return sorted_confidence, predicted_confidence, predicted_class
+
+
+def get_all_levels(df, df_naf, col="APE_NIV5_pred", revision=None):
+    """
+    Get all levels of the NAF code from the dataframe.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the NAF codes.
+        df_naf (pd.DataFrame): DataFrame containing the NAF data.
+
+    Returns:
+        pd.DataFrame: DataFrame with all levels of the NAF code.
+    """
+    df = df[[col]]
+
+    if df[col].dtype == "int64":
+        inv_ape_niv5_mapping = get_inv_mapping(revision=revision)
+        df[col] = df[col].map(inv_ape_niv5_mapping)
+
+    df_full = df.merge(df_naf, left_on=col, right_on="APE_NIV5").drop(columns=[col, "LIB_NIV5"])
+
+    return df_full
+
+
+def get_pred_nafs(predicted_class, revision):
+    """
+    Get the predicted NAF codes from the predicted class.
+
+    Args:
+        predicted_class (torch.Tensor): Predicted class tensor.
+        revision (str): The revision of the NAF code.
+            Must be either "NAF2008" or "NAF2025".
+
+    Returns:
+        pd.DataFrame: DataFrame with the predicted NAF codes.
+    """
+    inv_ape_niv5_mapping = get_inv_mapping(revision=revision)
+    df_pred = pd.DataFrame(predicted_class.numpy(), columns=["APE_NIV5_pred"])
+    df_pred["APE_NIV5_pred"] = df_pred["APE_NIV5_pred"].map(inv_ape_niv5_mapping)
+
+    return df_pred
