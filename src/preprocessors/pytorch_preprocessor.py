f3df6b8670b40ed4522ebbbdda7ad84d77d42017
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-05-15 17:15:05 +0000

    feat(api)!: preprocessing functions are encapsulated by PyTorch preprocessor class
    "packaged" in a sense
    to be called in api

--- src/preprocessors/pytorch_preprocessor.py
+++ src/preprocessors/pytorch_preprocessor.py
@@ -2,16 +2,32 @@
 PytorchPreprocessor class.
 """
 
+import string
 from typing import List, Optional, Tuple
 
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from utils.data import categorize_surface
-from utils.mappings import SURFACE_COLS, mappings
-from utils.preprocess import clean_text_feature
+from mappings import SURFACE_COLS, mappings
 
 from .base import Preprocessor
+
+try:
+    import nltk
+    from nltk.corpus import stopwords as ntlk_stopwords
+    from nltk.stem.snowball import SnowballStemmer
+
+    HAS_NLTK = True
+except ImportError:
+    HAS_NLTK = False
+
+try:
+    import unidecode
+
+    HAS_UNIDECODE = True
+except ImportError:
+    HAS_UNIDECODE = False
 
 
 class PytorchPreprocessor(Preprocessor):
@@ -19,8 +35,8 @@ class PytorchPreprocessor(Preprocessor):
     FastTextPreprocessor class.
     """
 
+    @staticmethod
     def clean_textual_features(
-        self,
         df: pd.DataFrame,
         textual_features: List[str],
     ) -> pd.DataFrame:
@@ -38,7 +54,9 @@ class PytorchPreprocessor(Preprocessor):
             df (pd.DataFrame): DataFrame.
         """
         for textual_feature in textual_features:
-            df[textual_feature] = clean_text_feature(df[textual_feature], remove_stop_words=True)
+            df[textual_feature] = PytorchPreprocessor.clean_text_feature(
+                df[textual_feature], remove_stop_words=True
+            )
             df[textual_feature] = df[textual_feature].str.replace(
                 "nan", ""
             )  # empty string instead of "nan" (nothing will be added to the libelle)
@@ -66,7 +84,7 @@ class PytorchPreprocessor(Preprocessor):
         for surface_col in SURFACE_COLS:
             if surface_col in categorical_features:
                 df[surface_col] = df[surface_col].astype(float)
-                df = categorize_surface(df, surface_col)
+                df = PytorchPreprocessor.categorize_surface(df, surface_col)
         for variable in categorical_features:
             if variable not in SURFACE_COLS:  # Mapping already done for this variable
                 if len(set(df[variable].unique()) - set(mappings[variable].keys())) > 0:
@@ -84,6 +102,112 @@ class PytorchPreprocessor(Preprocessor):
             df[y] = df[y].apply(mappings[y].get)
 
         return df
+
+    @staticmethod
+    def clean_text_feature(text: list[str], remove_stop_words=True):
+        """
+        Cleans a text feature.
+
+        Args:
+            text (list[str]): List of text descriptions.
+            remove_stop_words (bool): If True, remove stopwords.
+
+        Returns:
+            list[str]: List of cleaned text descriptions.
+
+        """
+        if not HAS_NLTK:
+            raise ImportError(
+                "nltk is not installed and is required for preprocessing. Run 'pip install torchFastText[preprocess]'."
+            )
+        if not HAS_UNIDECODE:
+            raise ImportError(
+                "unidecode is not installed and is required for preprocessing. Run 'pip install torchFastText[preprocess]'."
+            )
+
+        # Define stopwords and stemmer
+
+        nltk.download("stopwords", quiet=True)
+        stopwords = tuple(ntlk_stopwords.words("french")) + tuple(string.ascii_lowercase)
+        stemmer = SnowballStemmer(language="french")
+
+        # Remove of accented characters
+        text = np.vectorize(unidecode.unidecode)(np.array(text))
+
+        # To lowercase
+        text = np.char.lower(text)
+
+        # Remove one letter words
+        def mylambda(x):
+            return " ".join([w for w in x.split() if len(w) > 1])
+
+        text = np.vectorize(mylambda)(text)
+
+        # Remove duplicate words and stopwords in texts
+        # Stem words
+        libs_token = [lib.split() for lib in text.tolist()]
+        libs_token = [
+            sorted(set(libs_token[i]), key=libs_token[i].index) for i in range(len(libs_token))
+        ]
+        if remove_stop_words:
+            text = [
+                " ".join([stemmer.stem(word) for word in libs_token[i] if word not in stopwords])
+                for i in range(len(libs_token))
+            ]
+        else:
+            text = [
+                " ".join([stemmer.stem(word) for word in libs_token[i]])
+                for i in range(len(libs_token))
+            ]
+
+        # Return clean DataFrame
+        return text
+
+    @staticmethod
+    def categorize_surface(
+        df: pd.DataFrame, surface_feature_name: str, like_sirene_3: bool = True
+    ) -> pd.DataFrame:
+        """
+        Categorize the surface of the activity.
+
+        Args:
+            df (pd.DataFrame): DataFrame to categorize.
+            surface_feature_name (str): Name of the surface feature.
+            like_sirene_3 (bool): If True, categorize like Sirene 3.
+
+        Returns:
+            pd.DataFrame: DataFrame with a new column "surf_cat".
+        """
+        df_copy = df.copy()
+        # Check surface feature exists
+        if surface_feature_name not in df.columns:
+            raise ValueError(f"Surface feature {surface_feature_name} not found in DataFrame.")
+        # Check surface feature is a float variable
+        if not (pd.api.types.is_float_dtype(df[surface_feature_name])):
+            raise ValueError(f"Surface feature {surface_feature_name} must be a float variable.")
+
+        if like_sirene_3:
+            # Categorize the surface
+            df_copy["surf_cat"] = pd.cut(
+                df_copy[surface_feature_name],
+                bins=[0, 120, 400, 2500, np.inf],
+                labels=["1", "2", "3", "4"],
+            ).astype(str)
+        else:
+            # Log transform the surface
+            df_copy["surf_log"] = np.log(df[surface_feature_name])
+
+            # Categorize the surface
+            df_copy["surf_cat"] = pd.cut(
+                df_copy.surf_log,
+                bins=[0, 3, 4, 5, 12],
+                labels=["1", "2", "3", "4"],
+            ).astype(str)
+
+        df_copy[surface_feature_name] = df_copy["surf_cat"].replace("nan", "0")
+        df_copy[surface_feature_name] = df_copy[surface_feature_name].astype(int)
+        df_copy = df_copy.drop(columns=["surf_log", "surf_cat"], errors="ignore")
+        return df_copy
 
     def preprocess_for_model(
         self,
@@ -117,7 +241,7 @@ class PytorchPreprocessor(Preprocessor):
         """
 
         # Clean text + textual features, add textual features at the end of the text (libelle)
-        df[text_feature] = clean_text_feature(df[text_feature], remove_stop_words=True)
+        df[text_feature] = self.clean_text_feature(df[text_feature], remove_stop_words=True)
         df = self.clean_textual_features(df, textual_features)
         df[text_feature] = df[text_feature] + df[textual_features].apply(
             lambda x: "".join(x), axis=1
