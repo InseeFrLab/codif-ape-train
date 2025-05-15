77f56aea8c8b371d91fa7ad87cadf3236d559c63
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-05-15 17:21:32 +0000

    refactor(api)!: delete as moved func to PyTorch preprocessor as static
    related to d2c8c5e1fbe6aa2977e29d30b4481f0048247bfd

--- src/utils/data.py
+++ src/utils/data.py
@@ -251,52 +251,6 @@ def get_processed_data(revision):
     return df_train, df_val, df_test
 
 
-def categorize_surface(
-    df: pd.DataFrame, surface_feature_name: int, like_sirene_3: bool = True
-) -> pd.DataFrame:
-    """
-    Categorize the surface of the activity.
-
-    Args:
-        df (pd.DataFrame): DataFrame to categorize.
-        surface_feature_name (str): Name of the surface feature.
-        like_sirene_3 (bool): If True, categorize like Sirene 3.
-
-    Returns:
-        pd.DataFrame: DataFrame with a new column "surf_cat".
-    """
-    df_copy = df.copy()
-    # Check surface feature exists
-    if surface_feature_name not in df.columns:
-        raise ValueError(f"Surface feature {surface_feature_name} not found in DataFrame.")
-    # Check surface feature is a float variable
-    if not (pd.api.types.is_float_dtype(df[surface_feature_name])):
-        raise ValueError(f"Surface feature {surface_feature_name} must be a float variable.")
-
-    if like_sirene_3:
-        # Categorize the surface
-        df_copy["surf_cat"] = pd.cut(
-            df_copy[surface_feature_name],
-            bins=[0, 120, 400, 2500, np.inf],
-            labels=["1", "2", "3", "4"],
-        ).astype(str)
-    else:
-        # Log transform the surface
-        df_copy["surf_log"] = np.log(df[surface_feature_name])
-
-        # Categorize the surface
-        df_copy["surf_cat"] = pd.cut(
-            df_copy.surf_log,
-            bins=[0, 3, 4, 5, 12],
-            labels=["1", "2", "3", "4"],
-        ).astype(str)
-
-    df_copy[surface_feature_name] = df_copy["surf_cat"].replace("nan", "0")
-    df_copy[surface_feature_name] = df_copy[surface_feature_name].astype(int)
-    df_copy = df_copy.drop(columns=["surf_log", "surf_cat"], errors="ignore")
-    return df_copy
-
-
 def get_df_naf(
     revision: str,
 ) -> pd.DataFrame:
