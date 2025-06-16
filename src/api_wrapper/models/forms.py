04d1de06321e218bdd635c00c98402ddb13896eb
Thomas FARIA <thomas.faria@insee.fr>
2025-06-16 12:28:49 +0000

    fix: use regex to validate input digits
    we want to keep input string and check if the string indeed contains only digits

--- src/api_wrapper/models/forms.py
+++ src/api_wrapper/models/forms.py
@@ -1,6 +1,7 @@
+import re
 from typing import Optional
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, validator
 
 from ..constants.model import VALID_ACTIV_PERM, VALID_SURFACE, VALID_TYPE_FORM
 
@@ -15,32 +16,34 @@ class SingleForm(BaseModel):
     cj: Optional[str] = None
     activity_permanence_status: Optional[str] = None
 
-    @field_validator("type_form")
-    def validate_type_form(cls, v):
+    @validator("type_form")
+    def validate_type_form(cls, v: str) -> str:
         if (v is not None) and (v not in VALID_TYPE_FORM):
             raise ValueError(f"Invalid type_form '{v}', must be one of {VALID_TYPE_FORM}")
         return v
 
-    @field_validator("nature")
-    def validate_nature(cls, v):
-        if (v is not None) and (not v.isdigit() or len(v) != 2):
-            raise ValueError("nature must be a two-digit number (e.g., '01')")
+    @validator("nature")
+    def validate_nature(cls, v: str) -> str:
+        if v is not None:
+            if (not re.fullmatch(r"\d+", v)) or (len(v) != 2):
+                raise ValueError("nature must be a two-digit number (e.g., '01')")
         return v
 
-    @field_validator("surface")
-    def validate_surface(cls, v):
+    @validator("surface")
+    def validate_surface(cls, v: str) -> str:
         if (v is not None) and (v not in VALID_SURFACE):
             raise ValueError(f"Invalid surface '{v}', must be one of {VALID_SURFACE}")
         return v
 
-    @field_validator("cj")
-    def validate_cj(cls, v):
-        if (v is not None) and (not v.isdigit() or len(v) != 2):
-            raise ValueError("cj must be a 4-digit number (e.g., '5499')")
+    @validator("cj")
+    def validate_cj(cls, v: str) -> str:
+        if v is not None:
+            if (not re.fullmatch(r"\d+", v)) or (len(v) != 4):
+                raise ValueError("cj must be a 4-digit number (e.g., '5499')")
         return v
 
-    @field_validator("activity_permanence_status")
-    def validate_activity_permanence_status(cls, v):
+    @validator("activity_permanence_status")
+    def validate_activity_permanence_status(cls, v: str) -> str:
         if (v is not None) and (v not in VALID_ACTIV_PERM):
             raise ValueError(f"Invalid surface '{v}', must be one of {VALID_ACTIV_PERM}")
         return v
