e7f500f6f7726e644095b963e10c4673f8280b75
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-07-15 11:52:56 +0000

    chore!: remove fasttext legacy

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,5 +2,4 @@
 Init file for the models package.
 """
 
-from .fasttext_classifier import FastTextWrapper as FastTextWrapper
 from .torch_fasttext import torchFastTextClassifier as torchFastTextClassifier
