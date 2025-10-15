f568e3532407f1451d58fa820f792524f5e81a0c
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-10-15 12:08:39 +0000

    chore: add hydra full error as env variable

--- setup.sh
+++ setup.sh
@@ -9,3 +9,4 @@ export AWS_SESSION_TOKEN=""
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
 
 export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr/
+export HYDRA_FULL_ERROR=1
