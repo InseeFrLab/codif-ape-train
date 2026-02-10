1707536f26b77e487a6986b0cab023673f77f10c
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2026-02-10 10:03:41 +0000

    feat!: package layout + add project.script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+uv lock --upgrade
 uv sync
 uv run pre-commit install
 
 export AWS_SESSION_TOKEN=""
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-
-export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr/
 export HYDRA_FULL_ERROR=1
